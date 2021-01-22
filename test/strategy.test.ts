6165fd64d4f2d7094d70072a2cd9deb9a3277256
Nishant Neeraj <nishant.neeraj@gmail.com>
2021-01-22 09:11:02 +0530

    🏷️ Fixed typescript errors in tests

--- test/strategy.test.ts
+++ test/strategy.test.ts
@@ -2,6 +2,7 @@ import * as chai from 'chai';
 // import * as chaiAsPromise from 'chai-as-promised';
 import Strategy from '../src';
 import rewire from 'rewire';
+import CognitoExpressConfig from '../src/CognitoExpressConfig';
 
 const expect = chai.expect;
 const strategyConfig = {
@@ -10,16 +11,27 @@ const strategyConfig = {
   tokenUse: 'access', //Possible Values: access | id
   tokenExpiration: 3600000, //Up to default expiration of 1 hour (4000 ms)
 };
+
+//eslint-disable-next-line
 const token =
-  'eyJraWQiOiJVb0dmOG95UTNrbXB6WlZFWmZxS3RDQ0hIQ3lVNUo3RnV6dFRWSmxRNGtZPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIzNzZkOWY5MC04MjkzLTRhODMtYjA0Yi0zYWRjZTJlYTkzZTAiLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6ImF3cy5jb2duaXRvLnNpZ25pbi51c2VyLmFkbWluIiwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tXC91cy1lYXN0LTFfZFhsRmVmNzN0IiwiZXhwIjoxNTAzNDA5NjM5LCJpYXQiOjE1MDM0MDYwMzksImp0aSI6ImE1YjhjNWZhLWJiNDgtNGNkYi1hOTZiLTk4MTc3NjEyN2I1NyIsImNsaWVudF9pZCI6IjZ1ZDZ1cG9xaG5kYzd2Y3YxMDR2bG5hbHBuIiwidXNlcm5hbWUiOiJ2aXNhb2ZmaWNlcjFAY25hLmdvdiJ9.FL69JLtHFCeBU1nKYRyZYBBEO1TVaDCCuRTvrw_piSqLM_DQ2sUUJQYr9Ww3DudaTcjHDkV_rJufb3BzAfVMG0pOkZit6JkTexoCOHzXRNn0Bgmk87vVB52H2zOe6hRw_pF90UUmg6q9kYb2zapQg_-3ZvE1TiDQ0v45RZDKUHqWUFka3L3q6_uDHfCFrTr_APBN7y2u_w10jaL75CQ4h3RePOxKTiySjLq0FsFnl576eu_ZkoliUmApPCD618dRbu4kybCizbvYLLx8x-vM3CvPjnv_bXL1FtOWjzjSifuM5Hjnr1FxozCBQipBZ7X6vQBKy5hNX17O3sBt9KXXlg';
+  'eyJraWQiOiJVb0dmOG95UTNrbXB6WlZFWmZxS3RDQ0hIQ3lVNUo3RnV6dFRWSmxRNGtZPSIsImFsZyI6IlJTM' +
+  'jU2In0.eyJzdWIiOiIzNzZkOWY5MC04MjkzLTRhODMtYjA0Yi0zYWRjZTJlYTkzZTAiLCJ0b2tlbl91c2UiOi' +
+  'JhY2Nlc3MiLCJzY29wZSI6ImF3cy5jb2duaXRvLnNpZ25pbi51c2VyLmFkbWluIiwiaXNzIjoiaHR0cHM6XC9' +
+  'cL2NvZ25pdG8taWRwLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tXC91cy1lYXN0LTFfZFhsRmVmNzN0IiwiZXhw' +
+  'IjoxNTAzNDA5NjM5LCJpYXQiOjE1MDM0MDYwMzksImp0aSI6ImE1YjhjNWZhLWJiNDgtNGNkYi1hOTZiLTk4M' +
+  'Tc3NjEyN2I1NyIsImNsaWVudF9pZCI6IjZ1ZDZ1cG9xaG5kYzd2Y3YxMDR2bG5hbHBuIiwidXNlcm5hbWUiOi' +
+  'J2aXNhb2ZmaWNlcjFAY25hLmdvdiJ9.FL69JLtHFCeBU1nKYRyZYBBEO1TVaDCCuRTvrw_piSqLM_DQ2sUUJQ' +
+  'Yr9Ww3DudaTcjHDkV_rJufb3BzAfVMG0pOkZit6JkTexoCOHzXRNn0Bgmk87vVB52H2zOe6hRw_pF90UUmg6q' +
+  '9kYb2zapQg_-3ZvE1TiDQ0v45RZDKUHqWUFka3L3q6_uDHfCFrTr_APBN7y2u_w10jaL75CQ4h3RePOxKTiyS' +
+  'jLq0FsFnl576eu_ZkoliUmApPCD618dRbu4kybCizbvYLLx8x-vM3CvPjnv_bXL1FtOWjzjSifuM5Hjnr1Fxo' +
+  'zCBQipBZ7X6vQBKy5hNX17O3sBt9KXXlg';
 
 chai.should();
 
 describe('Strategy Negative Scenarios', () => {
   it('should not have config undefined', function () {
     expect(function () {
-      //@ts-ignore
-      new Strategy();
+      new Strategy((undefined as unknown) as CognitoExpressConfig);
     }).to.throw(
       TypeError,
       'Options not found. Please refer to README for usage example at https://github.com/ghdna/cognito-express',
@@ -28,12 +40,11 @@ describe('Strategy Negative Scenarios', () => {
 
   it('should not have token use undefined', function () {
     expect(function () {
-      //@ts-ignore
       new Strategy({
         region: 'us-east-1',
         cognitoUserPoolId: 'us-east-1_PNBFQ9W7X',
         tokenExpiration: 3600000, //Up to default expiration of 1 hour (4000 ms)
-      });
+      } as CognitoExpressConfig);
     }).to.throw(TypeError, "Token use not specified in constructor. Possible values 'access' | 'id'");
   });
 
@@ -50,23 +61,21 @@ describe('Strategy Negative Scenarios', () => {
 
   it('should not have user pool undefined', function () {
     expect(function () {
-      //@ts-ignore
       new Strategy({
         region: 'us-east-1',
         tokenUse: 'access', //Possible Values: access | id
         tokenExpiration: 3600000, //Up to default expiration of 1 hour (4000 ms)
-      });
+      } as CognitoExpressConfig);
     }).to.throw(TypeError, 'Cognito User Pool ID is not specified in constructor');
   });
 
   it('should not have Region undefined', function () {
     expect(function () {
-      //@ts-ignore
       new Strategy({
         cognitoUserPoolId: 'us-east-1_PNBFQ9W7X',
         tokenUse: 'access', //Possible Values: access | id
         tokenExpiration: 3600000, //Up to default expiration of 1 hour (4000 ms)
-      });
+      } as CognitoExpressConfig);
     }).to.throw(TypeError, 'AWS Region not specified in constructor');
   });
 });
@@ -74,7 +83,6 @@ describe('Strategy Negative Scenarios', () => {
 describe('Strategy Positive Scenarios', () => {
   const app = rewire('../src/index.ts');
   let strategy: Strategy;
-  beforeEach(() => {});
 
   it('should check if GeneratePem function exists', () => {
     strategy = new Strategy(strategyConfig);
